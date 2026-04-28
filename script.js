daceecc8bf16aa9f5358c08efb21b54ddc3ea6af
Lunar <110432826+celestial-chan@users.noreply.github.com>
2026-04-29 09:16:59 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
 // ================= PAGE SWITCH =================
-
 function switchPage(pageId) {
-  const pages = document.querySelectorAll('.page');
-
-  pages.forEach(p => p.classList.remove('active'));
+  document.querySelectorAll('.page').forEach(p => {
+    p.classList.remove('active');
+  });
 
   document.getElementById(pageId).classList.add('active');
 }
